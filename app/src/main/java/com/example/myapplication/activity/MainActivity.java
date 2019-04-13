b4b805aedc717214f73c8857c18cc34e3b1d37fe
nmchi96 <nmchi96@gmail.com>
2019-04-13 10:43:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.myapplication.activity;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.myapplication.R;
+import com.example.myapplication.adapter.LoaispAdapter;
+import com.example.myapplication.adapter.SanphamAdapter;
+import com.example.myapplication.model.Loaisp;
+import com.example.myapplication.model.Sanpham;
+import com.example.myapplication.ultil.CheckConnection;
+import com.example.myapplication.ultil.Server;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewmanhinhchinh;
+    BottomNavigationView navigationView;
+    ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
+    ArrayList<Loaisp> mangloaisp;
+    LoaispAdapter loaispAdapter;
+    int id = 0;
+    String tenloaisp = "";
+    String hinhanhloaisp = "";
+    ArrayList<Sanpham> mangsanpham;
+    SanphamAdapter sanphamAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionBar();
+            ActionViewFlipper();
+            GetDuLieuLoaisp();
+            GetDuLieuSPMoiNhat();
+        }else{
+            CheckConnection.ShowToast_Short(getApplicationContext(), "Bạn hãy kiểm tra lại kết nối internet.");
+            finish();
+        }
+    }
+
+    private void GetDuLieuSPMoiNhat() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.Duongdanspmoinhat, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response != null){
+                    int ID = 0;
+                    String Tensanpham = "";
+                    Integer Giasanpham = 0;
+                    String Hinhanhsanpham = "";
+                    String Motasanpham = "";
+                    int IDsanpham = 0;
+                    for (int i = 0; i<response.length(); i++){
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            ID = jsonObject.getInt("id");
+                            Tensanpham = jsonObject.getString("tensp");
+                            Giasanpham = jsonObject.getInt("giasp");
+                            Hinhanhsanpham = jsonObject.getString("hinhanhsp");
+                            Motasanpham = jsonObject.getString("motasp");
+                            IDsanpham = jsonObject.getInt("idsanpham");
+                            mangsanpham.add(new Sanpham(ID, Tensanpham, Giasanpham, Hinhanhsanpham, Motasanpham, IDsanpham));
+                            sanphamAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void GetDuLieuLoaisp() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.DuongdanLoaisp, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response != null){
+                    for (int i = 0; i < response.length(); i++){
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            id = jsonObject.getInt("id");
+                            tenloaisp = jsonObject.getString("tenloaisp");
+                            hinhanhloaisp = jsonObject.getString("hinhanhloaisp");
+                            mangloaisp.add(new Loaisp(id, tenloaisp, hinhanhloaisp));
+                            loaispAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mangloaisp.add(3, new Loaisp(0, "Liên hệ", "https://cdn.tuoitre.vn/thumb_w/640/2018/11/3/photo-1-15412167047771445781955.jpg"));
+                    mangloaisp.add(4, new Loaisp(0, "Thông tin", "http://file.vforum.vn/hinh/2015/05/dien-thoai-chup-hinh-dep-nhat-iphone-6-plus.jpg"));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                CheckConnection.ShowToast_Short(getApplicationContext(), error.toString());
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://dienthoaivivo.com/wp-content/uploads/2015/11/x5-pro-1.png");
+        mangquangcao.add("http://www.istarmobile.vn/images/news/1545.jpg");
+        for (int i = 0; i<mangquangcao.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        //bắt sự kiện viewFlipper tự chạy
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        //hieu ung
+        Animation animation_slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation animation_slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(animation_slide_in);
+        viewFlipper.setOutAnimation(animation_slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //nhay ra giua
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewmanhinhchinh = findViewById(R.id.recyclerview);
+
+        //navigationView = findViewById(R.id.navigationview);
+        listViewmanhinhchinh = findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        mangloaisp = new ArrayList<>();
+        mangloaisp.add(0, new Loaisp(0, "Trang chính", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn3_MvlCdpciUaje9g4O45h64DwookJb724SXCyBqY1TRGclne"));
+        loaispAdapter = new LoaispAdapter(mangloaisp, getApplicationContext());
+        listViewmanhinhchinh.setAdapter(loaispAdapter);
+        mangsanpham = new ArrayList<>();
+        sanphamAdapter = new SanphamAdapter(getApplicationContext(), mangsanpham);
+        recyclerViewmanhinhchinh.setHasFixedSize(true);
+        recyclerViewmanhinhchinh.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+        recyclerViewmanhinhchinh.setAdapter(sanphamAdapter);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/ultil/Server.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ultil;
+
+public class Server {
+    public static String localhost = "10.16.72.52";
+    public static String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
+    public static String Duongdanspmoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
+}
